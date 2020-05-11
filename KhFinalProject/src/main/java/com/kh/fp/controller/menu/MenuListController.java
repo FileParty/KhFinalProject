50f5e80b656e43d1acc6f152caea69ace659b8b1
user2 <user2@user2-PC>
2020-05-11 18:18:38 +0900

    #기본작업중 -bs

--- KhFinalProject/src/main/java/com/kh/fp/controller/menu/MenuListController.java
+++ KhFinalProject/src/main/java/com/kh/fp/controller/menu/MenuListController.java
@@ -9,21 +9,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.kh.fp.model.vo.FoodCategory;
 
 @Controller
-public class FoodController {
+public class MenuListController {
 	
-	@RequestMapping("/food/foodlist.do")
+	
+	
+	@RequestMapping("/menu/menuList.do")
 	public String foodList(Model m) {
 		
 		FoodCategory category;
 		ArrayList<FoodCategory> list = new ArrayList<FoodCategory>();
 		
 		for(int i=0; i<20; i++) {
-			category = new FoodCategory("돌돌이 닭갈비", i, 244, 13000, 34);
+			category = new FoodCategory("돌돌이 닭갈비", i, 244, 13000, 34,"몰라",0,"ss","ss");
 			list.add(category);
 		}
 		
 		
 		m.addAttribute("list", list);
-		return "foodlist";
+		return "menu/menulist";
 	}
 }
--- KhFinalProject/src/main/java/com/kh/fp/model/vo/FoodCategory.java
+++ KhFinalProject/src/main/java/com/kh/fp/model/vo/FoodCategory.java
@@ -1,73 +1,22 @@
 package com.kh.fp.model.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FoodCategory {
 	
 	private String title;
-	private int star;
+	private double score;
 	private int reviewCount;
 	private int deleveryMinCharge;
 	private int deleveryTime;
-	
-	public FoodCategory() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public FoodCategory(String title, int star, int reviewCount, int deleveryMinCharge, int deleveryTime) {
-		super();
-		this.title = title;
-		this.star = star;
-		this.reviewCount = reviewCount;
-		this.deleveryMinCharge = deleveryMinCharge;
-		this.deleveryTime = deleveryTime;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public int getStar() {
-		return star;
-	}
-
-	public void setStar(int star) {
-		this.star = star;
-	}
-
-	public int getReviewCount() {
-		return reviewCount;
-	}
-
-	public void setReviewCount(int reviewCount) {
-		this.reviewCount = reviewCount;
-	}
-
-	public int getDeleveryMinCharge() {
-		return deleveryMinCharge;
-	}
-
-	public void setDeleveryMinCharge(int deleveryMinCharge) {
-		this.deleveryMinCharge = deleveryMinCharge;
-	}
-
-	public int getDeleveryTime() {
-		return deleveryTime;
-	}
-
-	public void setDeleveryTime(int deleveryTime) {
-		this.deleveryTime = deleveryTime;
-	}
-
-	@Override
-	public String toString() {
-		return "FoodCategory [title=" + title + ", star=" + star + ", reviewCount=" + reviewCount
-				+ ", deleveryMinCharge=" + deleveryMinCharge + ", deleveryTime=" + deleveryTime + "]";
-	}
-
-	
-	
+	private String category;
+	private double km;
+	private String logimg;
+	private String recentimg;
 	
 }
