6b43eebdb76b3065af60471803bc629e9627d544
user2 <user2@user2-PC>
2020-05-13 21:48:46 +0900

    #2020-05-13 mypage -ryucb

--- KhFinalProject/src/main/java/com/kh/fp/controller/mypage/UserMypage.java
+++ KhFinalProject/src/main/java/com/kh/fp/controller/mypage/UserMypage.java
@@ -1,10 +1,23 @@
 package com.kh.fp.controller.mypage;
 
+import static com.kh.fp.common.PageingFactory.PageBarFactory;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kh.fp.model.service.mypage.MypageService;
+
+import net.sf.json.JSON;
 
 @Controller
 public class UserMypage {
@@ -18,8 +31,42 @@ public class UserMypage {
 	}
 	
 	@RequestMapping("/mypage/orderHistory.do")
-	public String orderHistory() {
+	public String orderHistory(@RequestParam Map map, HttpSession session, Model m) {
+		
+//		session.getAttribute();
+		String userId = (String)session.getAttribute("userId");
+		
+		int cPage=1;
+		
+		try {
+			cPage = (int)map.get("cPage");
+		}catch(Exception e) {
+		}
+		int numPerPage = 5;
+		int totalData = service.getTotalCount(userId);
+		String url = "/mypage/mypage.do";
+		
+		List<Map<String, String>> list = service.selectOrder(userId, cPage, numPerPage);
+		
+		String pageBar = PageBarFactory(cPage, numPerPage, totalData, url);
+		
+		System.out.println(totalData);
+		
+		m.addAttribute("list", list);
+		m.addAttribute("pageBar", pageBar);
+		
+		
+				
 		return "mypage/mypage_orderHistory";
+	}
+	
+	@RequestMapping("/mypage/orderMenu.do")
+	@ResponseBody
+	public List<Map<String, String>> selectOrderMenu(int o_no) {
+		
+		List<Map<String, String>> list = service.selectOrderMenu(o_no);
+		
+		return list;
 	}
 	
 	@RequestMapping("/mypage/review.do")
--- KhFinalProject/src/main/java/com/kh/fp/model/dao/mypage/MypageDaoImpl.java
+++ KhFinalProject/src/main/java/com/kh/fp/model/dao/mypage/MypageDaoImpl.java
@@ -1,9 +1,28 @@
 package com.kh.fp.model.dao.mypage;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MypageDaoImpl implements MypageDao {
-	
+
+	@Override
+	public int getTotalCount(SqlSessionTemplate session, String userId) {
+		return session.selectOne("mypage.totalCount", userId);
+	}
+
+	@Override
+	public List<Map<String, String>> selectOrder(SqlSessionTemplate session, String userId, int cPage, int numPerPage) {
+		return session.selectList("mypage.selectOrder", userId, new RowBounds((cPage-1)*numPerPage,numPerPage));
+	}
+
+	@Override
+	public List<Map<String, String>> selectOrderMenu(SqlSessionTemplate session, int o_no) {
+		return session.selectList("mypage.selectOrderMenu", o_no);
+	}
 
 }
