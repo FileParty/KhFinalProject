4d0d180ed19cacfdcde7f3005ad06e4cacb1b142
GB <GB@220.94.141.194>
2020-05-15 03:02:53 +0900

    #20200515 mypageRe -ryucb

--- KhFinalProject/src/main/java/com/kh/fp/model/service/mypage/MypageServiceImpl.java
+++ KhFinalProject/src/main/java/com/kh/fp/model/service/mypage/MypageServiceImpl.java
@@ -19,13 +19,13 @@ public class MypageServiceImpl implements MypageService {
 	private SqlSessionTemplate session;
 
 	@Override
-	public int getTotalCount(String userId) {
-		return dao.getTotalCount(session, userId);
+	public int getTotalCount(int o_no) {
+		return dao.getTotalCount(session, o_no);
 	}
 
 	@Override
-	public List<Map<String, String>> selectOrder(String userId, int cPage, int numPerPage) {
-		return dao.selectOrder(session, userId, cPage, numPerPage);
+	public List<Map<String, String>> selectOrder(int o_no, int cPage, int numPerPage) {
+		return dao.selectOrder(session, o_no, cPage, numPerPage);
 	}
 
 	@Override
@@ -33,6 +33,48 @@ public class MypageServiceImpl implements MypageService {
 		// TODO Auto-generated method stub
 		return dao.selectOrderMenu(session, o_no);
 	}
+
+	@Override
+	public int reviewTotalCount(int m_no) {
+		return dao.reviewTotalCount(session, m_no);
+	
+	}
+
+	@Override
+	public List<Map<String, String>> selectReview(int m_no, int cPage, int numPerPage) {
+		return dao.selectReview(session, m_no, cPage, numPerPage);
+	}
+
+	@Override
+	public List<String> selectReviewImg(Map<String, String> map) {
+		return dao.selectReviewImg(session, map);
+	}
+
+	@Override
+	public List<Map<String, String>> selectPrefer(int m_no) {
+		return dao.selectPrefer(session, m_no);
+	}
+
+	@Override
+	public int delectWatchList(Map<String, String> map) {
+		return dao.delectWatchList(session, map);
+	}
+
+	@Override
+	public List<Map<String, String>> selectCoupon(int m_no) {
+		return dao.selectCoupon(session, m_no);
+	}
+
+	@Override
+	public int memberUpdate(Map<String, String> map) {
+		return dao.memberUpdate(session, map);
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 }
