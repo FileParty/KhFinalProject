854d8826ade5e6739131e04319677299bcc7779c
user2 <user2@user2-PC>
2020-06-01 20:18:11 +0900

    #100 스케쥴러 작성중 - IS

---
+++ KhFinalProject/src/main/java/com/kh/fp/common/controller/scheduler/CouponSchedulerController.java
@@ -0,0 +1,20 @@
+package com.kh.fp.common.controller.scheduler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.kh.fp.common.service.scheduler.CouponSchedulerService;
+
+@Component
+public class CouponSchedulerController {
+	
+	@Autowired
+	private CouponSchedulerService service;
+	
+	@Scheduled(fixedDelay=5000)
+	public void couponDateCheck() {
+		
+	}
+
+}
---
+++ KhFinalProject/src/main/java/com/kh/fp/common/service/scheduler/CouponSchedulerService.java
@@ -0,0 +1,5 @@
+package com.kh.fp.common.service.scheduler;
+
+public class CouponSchedulerService {
+
+}
